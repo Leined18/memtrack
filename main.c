1819e6c816e5efe950bc5796d1a4ef9dc5e96c10
Leined18 <erdanielmarciano@hotmail.com>
2024-11-15 19:22:03 +0100

    chaos funtion upgraded

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: danpalac <danpalac@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 01:12:30 by danpalac          #+#    #+#             */
-/*   Updated: 2024/11/15 17:24:33 by danpalac         ###   ########.fr       */
+/*   Updated: 2024/11/15 19:19:35 by danpalac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,26 @@ int	main(void)
 {
 	t_mt	*stacka;
 	t_mt	*stackb;
+	t_mt	*tmp;
 
 	stacka = NULL;
 	stackb = NULL;
-	ft_mtpush_data_back(&stacka, "miau");
-	ft_mtpush_data_back(&stacka, "pi");
-	ft_mtpush_data_back(&stacka, "mu");
-	ft_mtpush_data_back(&stacka, "le");
-	ft_mtpush_data_back(&stacka, "asd");
-	ft_mtpush_data_back(&stacka, "pi");
-	ft_mtpush_data_back(&stackb, "pi");
-	ft_mtpush_data_back(&stackb, "po");
-	ft_mtprint(stacka, 1, " ");
-	ft_mtprint(stackb, 1, " ");
-	ft_mtreplace(stacka, "pi", "po", ft_memcmp, 2);
-	ft_mtprint(stacka, 1, " ");
-	ft_mtprint(stackb, 1, " ");
-	ft_mtreplace_all(&stacka, &stackb);
-	ft_mtprint(stacka, 1, " ");
-	ft_mtprint(stackb, 1, " ");
+	tmp = chaosmatrix(0, 0, 1);
+	ft_mtprint(tmp, 0, " ");
+	ft_mtpush_data(&stacka, "miau");
+	ft_mtpush_data(&stacka, "guau");
+	tmp = chaosmatrix(0, 0, 1);
+	ft_mtprint(tmp, 0, " ");
+	ft_mterase(&stacka);
+	tmp = chaosmatrix(0, 0, 1);
+	ft_mtprint(tmp, 0, " ");
+	(void)stackb;
+	ft_mtmigrate(&stacka, &stackb);
+	ft_mtprint(stackb, 1, " -> ");
+	tmp = (t_mt *)chaosmatrix(0, 0, 1);
+	ft_mtprint(tmp, 0, " ");
+	chaosmatrix(0, 0, -1);
+	tmp = chaosmatrix(0, 0, 1);
+	ft_mtprint(tmp, 0, " ");
 	return (0);
 }
