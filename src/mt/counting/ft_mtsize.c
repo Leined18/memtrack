2a6a0ed7b92dce0b37f76bc019b727f44b236d7a
Leined <erdanielmarciano@hotmail.com>
2025-01-28 11:47:28 +0100

    size

--- src/mt/counting/ft_mtsize.c
+++ src/mt/counting/ft_mtsize.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_mtsize.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: danpalac <danpalac@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: danpalac <danpalac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 18:19:01 by danpalac          #+#    #+#             */
-/*   Updated: 2025/01/01 18:20:06 by danpalac         ###   ########.fr       */
+/*   Updated: 2025/01/28 11:43:49 by danpalac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,13 @@
 int	ft_mtsize(t_mt *node)
 {
 	int	size;
+	int	i;
 
-	size = 0;
 	if (!node)
 		return (0);
+	i = 0;
 	size = 1;
-	for (int i = 0; i < MAX_DIRECTIONS; i++)
-		size += ft_mtsize(node->vect[i]);
+	while (i < MAX_DIRECTIONS)
+		size += ft_mtsize(node->vect[i++]);
 	return (size);
 }
