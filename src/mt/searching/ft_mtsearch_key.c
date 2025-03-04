8bec3fb5eb0d8f872fef775c3619b5e6cd429953
MiguelViHe <miguelvidalhernando@gmail.com>
2025-03-04 18:55:48 +0100

    match_key - recolocación de comentario - borrar en el futuro

--- src/mt/searching/ft_mtsearch_key.c
+++ src/mt/searching/ft_mtsearch_key.c
@@ -6,23 +6,23 @@
 /*   By: mvidal-h <mvidal-h@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/16 15:55:25 by danpalac          #+#    #+#             */
-/*   Updated: 2025/02/11 17:54:55 by mvidal-h         ###   ########.fr       */
+/*   Updated: 2025/03/04 11:19:34 by mvidal-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mt.h"
 
-static int	match_key(t_mt *node, void *key)
-{
-	return (ft_strncmp(node->key, key, ft_strlen(key)) == 0 
-	&& (node->key[ft_strlen(key)] == '\0' || node->key[ft_strlen(key)] == '=')); 
-	/* Soluciona el UNSET TERM (buscaba cualquier cosa que empiece por TERM)
+/* Soluciona el UNSET TERM (buscaba cualquier cosa que empiece por TERM)
 	EL && QUIZAS PODEMOS CAMBIARLO A PASARLE LA FUNCION COMO PARAMETRO A ft_mtsearch_key.
 	yo tengo dos funciones ya creadas que buscan perfect_match:
 	   En cadenas normales (que acaban con '\0')
 	   Como cadenas de ENV (que tienen el formato PATH=algo)
 	De momento he dejado la version que comprueba las dos cosas a la vez.
-	*/
+*/
+static int	match_key(t_mt *node, void *key)
+{
+	return (ft_strncmp(node->key, key, ft_strlen(key)) == 0 
+	&& (node->key[ft_strlen(key)] == '\0' || node->key[ft_strlen(key)] == '=')); 
 }
 
 t_mt	*ft_mtsearch_key(t_mt *root, const char *key)
